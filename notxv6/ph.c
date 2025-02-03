87316ae43c54ccf107cfe703463db715cd960dde
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2025-02-03 19:09:31 +0800

    pass Task2:Using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,7 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t lock; // 声明一个锁 
 
 double
 now()
@@ -52,7 +53,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock); // 获取锁 
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock); // 释放锁
   }
 
 }
@@ -117,6 +120,8 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
+
+  pthread_mutex_init(&lock, NULL); // 初始化锁 
 
   //
   // first the puts
